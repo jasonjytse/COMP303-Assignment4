5ad3deea682788f1a83418fcd13e8ecede806c90
Jason Tse <59128576+jasonjytse@users.noreply.github.com>
2023-11-29 16:21:05 -0500

    Initial commit

---
+++ src/main/java/com/jt/JasonTseComp303Assignment4Application.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasonTseComp303Assignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasonTseComp303Assignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jt/JasonTseComp303Assignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasonTseComp303Assignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
